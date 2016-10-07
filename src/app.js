b9a4618f57751a938cea671a9ee162ee821bbbcf
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2016-10-07 19:15:10 +0200

    Prettified code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
+/*
+-----------------------------------------------------------------------------------
+|
+| Imports
+|
+-----------------------------------------------------------------------------------
+*/
+
 import Peer from 'simple-peer'
+
+/*
+-----------------------------------------------------------------------------------
+|
+| Simple peer connection
+|
+-----------------------------------------------------------------------------------
+*/
 
 var p = new Peer({ initiator: window.location.hash === '#1', trickle: false })
 
-p.on('error', (err) => console.log('error', err))
-
+// Peer receives a signal
+// A signal is fired if:
+// 1. Peer is the initiator
+// 2. Peer receives a signal from another peer
 p.on('signal', (data) => {
   console.log('SIGNAL', JSON.stringify(data))
   document.querySelector('#outgoing').textContent = JSON.stringify(data)
 })
 
-document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
   p.signal(JSON.parse(document.querySelector('#incoming').value))
 })
 
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
+// Peer establishes a connection after answering to a connection offer
+p.on('connect', () => {
+  p.send(`Random data: ${Math.random()}`)
 })
 
-p.on('data', function (data) {
+// Peer receives data after establishing a connection
+p.on('data', (data) => {
   console.log('data: ' + data)
 })
+
+// Error handler
+p.on('error', (err) => console.log('error', err))

b812f964e67945efcb81b021046e9dd7f99a5e6b
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2016-10-07 15:42:34 +0200

    Working peer2peer connection with simple-peer

--- dist/app.js
+++ dist/app.js
@@ -6104,52 +6104,29 @@ var _simplePeer2 = _interopRequireDefault(_simplePeer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var peer1 = new _simplePeer2.default({ initiator: true });
-var peer2 = new _simplePeer2.default();
+var p = new _simplePeer2.default({ initiator: window.location.hash === '#1', trickle: false });
 
-peer1.on('signal', function (data) {
-  // when peer1 has signaling data, give it to peer2 somehow
-  console.log('Recived signal on peer2: ' + data);
-  peer2.signal(data);
+p.on('error', function (err) {
+  return console.log('error', err);
 });
 
-peer2.on('signal', function (data) {
-  // when peer2 has signaling data, give it to peer1 somehow
-  console.log('Recived signal on peer2: ' + data);
-  peer1.signal(data);
+p.on('signal', function (data) {
+  console.log('SIGNAL', JSON.stringify(data));
+  document.querySelector('#outgoing').textContent = JSON.stringify(data);
 });
 
-peer1.on('connect', function () {
-  // wait for 'connect' event before using the data channel
-  peer1.send('hey peer2, how is it going?');
+document.querySelector('form').addEventListener('submit', function (ev) {
+  ev.preventDefault();
+  p.signal(JSON.parse(document.querySelector('#incoming').value));
 });
 
-peer2.on('data', function (data) {
-  // got a data channel message
-  console.log('got a message from peer1: ' + data);
+p.on('connect', function () {
+  console.log('CONNECT');
+  p.send('whatever' + Math.random());
 });
 
-// var p = new Peer({ initiator: window.location.hash === '#1', trickle: false })
-//
-// p.on('error', (err) => console.log('error', err))
-//
-// p.on('signal', (data) => {
-//   console.log('SIGNAL', JSON.stringify(data))
-//   document.querySelector('#outgoing').textContent = JSON.stringify(data)
-// })
-//
-// document.querySelector('form').addEventListener('submit', function (ev) {
-//   ev.preventDefault()
-//   p.signal(JSON.parse(document.querySelector('#incoming').value))
-// })
-//
-// p.on('connect', function () {
-//   console.log('CONNECT')
-//   p.send('whatever' + Math.random())
-// })
-//
-// p.on('data', function (data) {
-//   console.log('data: ' + data)
-// })
+p.on('data', function (data) {
+  console.log('data: ' + data);
+});
 
 },{"simple-peer":25}]},{},[28]);
--- src/app.js
+++ src/app.js
@@ -1,48 +1,24 @@
-import SimplePeer from 'simple-peer'
+import Peer from 'simple-peer'
 
-var peer1 = new SimplePeer({ initiator: true })
-var peer2 = new SimplePeer()
+var p = new Peer({ initiator: window.location.hash === '#1', trickle: false })
 
-peer1.on('signal', function (data) {
-  // when peer1 has signaling data, give it to peer2 somehow
-  console.log('Recived signal on peer2: ' + data)
-  peer2.signal(data)
+p.on('error', (err) => console.log('error', err))
+
+p.on('signal', (data) => {
+  console.log('SIGNAL', JSON.stringify(data))
+  document.querySelector('#outgoing').textContent = JSON.stringify(data)
 })
 
-peer2.on('signal', function (data) {
-  // when peer2 has signaling data, give it to peer1 somehow
-  console.log('Recived signal on peer2: ' + data)
-  peer1.signal(data)
+document.querySelector('form').addEventListener('submit', function (ev) {
+  ev.preventDefault()
+  p.signal(JSON.parse(document.querySelector('#incoming').value))
 })
 
-peer1.on('connect', function () {
-  // wait for 'connect' event before using the data channel
-  peer1.send('hey peer2, how is it going?')
+p.on('connect', function () {
+  console.log('CONNECT')
+  p.send('whatever' + Math.random())
 })
 
-peer2.on('data', function (data) {
-  // got a data channel message
-  console.log('got a message from peer1: ' + data)
+p.on('data', function (data) {
+  console.log('data: ' + data)
 })
-// var p = new Peer({ initiator: window.location.hash === '#1', trickle: false })
-//
-// p.on('error', (err) => console.log('error', err))
-//
-// p.on('signal', (data) => {
-//   console.log('SIGNAL', JSON.stringify(data))
-//   document.querySelector('#outgoing').textContent = JSON.stringify(data)
-// })
-//
-// document.querySelector('form').addEventListener('submit', function (ev) {
-//   ev.preventDefault()
-//   p.signal(JSON.parse(document.querySelector('#incoming').value))
-// })
-//
-// p.on('connect', function () {
-//   console.log('CONNECT')
-//   p.send('whatever' + Math.random())
-// })
-//
-// p.on('data', function (data) {
-//   console.log('data: ' + data)
-// })
